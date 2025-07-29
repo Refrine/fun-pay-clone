c90e60ae8395ff2cfde8a272875c70f6d3e7f146
Refrine <refrine123@bk.ru>
2025-07-29 15:28:44 +0300

    start

---
+++ funpay_clone/src/test/java/com/example/funpay_clone/FunpayCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.funpay_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunpayCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
